4e8a751f43ad614b72d75431889e2d5cd7fc344b
Yousef-I-Ahmed <yousef8720@outlook.com>
2026-03-01 18:34:22 +0200

    v1234

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
@@ -10,10 +10,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
